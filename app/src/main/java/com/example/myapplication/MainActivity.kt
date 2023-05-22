b31f5ca0e49e4dc637158d2648567d29195d62a6
VoronchikhinaAS <A.Voronchikhina@modulbank.ru>
2023-05-22 19:34:19 +0700

    Добавила onBackPressed

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.addCallback
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        onBackPressedDispatcher.addCallback {
+            if (!findNavController(R.id.navGraph).popBackStack()) {
+                Toast.makeText(this@MainActivity, "Activity", Toast.LENGTH_SHORT).show()
+                super.getOnBackPressedDispatcher().onBackPressed()
+            }
+        }
+    }
 }
